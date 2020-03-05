b36c0b67cf3fd6069b6d973c37cd8acf39d2a773
João Américo Caiafa <joaocaiafa@outlook.com>
2020-03-04 21:02:28 -0300

    Sprint 2

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import os
+import time
 import requests
-import json
+import csv
 
-headers = {"Authorization": "Bearer YOUR API KEY"}
+headers = {"Authorization": "Bearer 307450c7939fa297c4c9ad3659316664a5915a93"}
 
-
-def run_query(query):  #Metodo de chamada da API do GitHub
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+print("Iniciando processo")
+def run_query(json, headers):  # Função que executa uma request pela api graphql
+    request = requests.post('https://api.github.com/graphql', json=json,
+                            headers=headers)  # efetua uma requisição post determinando o json com a query recebida
+    while (request.status_code == 502):
+        time.sleep(2)
+        request = requests.post('https://api.github.com/graphql', json=json, headers=headers)
     if request.status_code == 200:
-        return request.json()
+        return request.json()  # json que retorna da requisição
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 
-# Query do GraphQL que procura os primeiros 100 repositorios com mais de 100 estrelas.
+# Query do GraphQL que procura os primeiros 1000 repositorios com mais de 100 estrelas.
 query = """
-{
-  search (query:"stars:>100",type: REPOSITORY, first:100) {
-    nodes {
+query example{
+  search (query:"stars:>100",type: REPOSITORY, first:20{AFTER}) {
+      pageInfo{
+       hasNextPage
+        endCursor
+      }
+      nodes {
         ... on Repository {
           nameWithOwner
           createdAt
@@ -38,9 +48,56 @@ query = """
           }
         }
       }
-  }
+    }
 }
 """
-result = run_query(query)  #Executar a Query
-nodes= result["data"]["search"]["nodes"] # separar a string para exibir apenas os nodes
-print(json.dumps(nodes, indent=4)) #exibir no console a string identada
+
+finalQuery = query.replace("{AFTER}", "")
+
+json = {
+    "query": finalQuery, "variables": {}
+}
+
+total_pages = 1
+print("Executando Query\n[", end = '')
+result = run_query(json, headers)  # Executar a Query
+nodes = result["data"]["search"]["nodes"]  # separar a string para exibir apenas os nodes
+next_page = result["data"]["search"]["pageInfo"]["hasNextPage"]
+
+page = 0
+while next_page and total_pages < 50:
+    total_pages += 1
+    cursor = result["data"]["search"]["pageInfo"]["endCursor"]
+    next_query = query.replace("{AFTER}", ", after: \"%s\"" % cursor)
+    json["query"] = next_query
+    result = run_query(json, headers)
+    nodes += result['data']['search']['nodes']
+    next_page = result["data"]["search"]["pageInfo"]["hasNextPage"]
+    print(".", end = '')
+print("]")
+
+if os.path.exists("repos.csv"):
+    os.remove("repos.csv")
+print("Criando arquivo CSV")
+file = open("repos.csv", 'wt')
+repositore = csv.writer(file)
+
+repositore.writerow(('nameWithOwner', 'createdAt', 'pullRequests/totalCount',  # Adicionando cabecalho
+                     'releases/totalCount', 'updatedAt', 'primaryLanguage/name',
+                     'closedIssues/totalCount', 'totalIssues/totalCount'))
+print("Salvando Repositorios:\n[", end = '')
+num = 0
+for node in nodes:
+    if node['primaryLanguage'] is None:
+        primaryLanguage = "null"
+    else:
+        primaryLanguage = str(node['primaryLanguage']['name'])
+        # Adicionando dados de cada repositorio
+        repositore.writerow((node['nameWithOwner'], node['createdAt'], str(node['pullRequests']['totalCount']),
+                             str(node['releases']['totalCount']), node['updatedAt'], primaryLanguage,
+                             str(node['closedIssues']['totalCount']), str(node['totalIssues']['totalCount'])))
+        ++num
+        if (num%10)==0:
+            print(".", end = '')
+print("]\nProcesso concluido")
+file.close()
